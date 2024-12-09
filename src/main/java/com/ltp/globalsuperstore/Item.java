b39b4eedfb61c019cff9addd7b00e2d4dc0b18db
Guga2111 <luisgosampaio@gmail.com>
2024-12-08 22:48:48 -0300

    upload project to git

---
+++ src/main/java/com/ltp/globalsuperstore/Item.java
@@ -0,0 +1,68 @@
+package com.ltp.globalsuperstore;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDate;
+
+public class Item {
+    private String category;
+    private String name;
+    private Double price;
+    private Double discount;
+    @DateTimeFormat(pattern = "dd-MM-yyyy")
+    private LocalDate date;
+
+    public Item() {
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Double getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(Double discount) {
+        this.discount = discount;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    @Override
+    public String toString() {
+        return "Item{" +
+                "category='" + category + '\'' +
+                ", name='" + name + '\'' +
+                ", price=" + price +
+                ", discount=" + discount +
+                ", date=" + date +
+                '}';
+    }
+}
